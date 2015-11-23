21610f77ec4abbf41f68541c8bdb929995703d33
Cezar Vinícius de O. Pretto <cezar@planosassessoria.com.br>
2015-11-23 16:12:22 -0200

    First commit

---
+++ src/js/directives/input.js
@@ -0,0 +1,46 @@
+/*
+Input types: text|email|tel|number|url|search|color|date|datetime|time|month|week
+*/
+
+(function() {
+
+  var types = 'text|password|email|tel|number|url|search|color|date|datetime|time|month|week|file'.split('|');
+
+  //todo: datalist
+
+  // generate directives
+  angular.forEach(types, function(type) {
+    var directiveName = 'editable'+type.charAt(0).toUpperCase() + type.slice(1);
+    angular.module('xeditable').directive(directiveName, ['editableDirectiveFactory',
+      function(editableDirectiveFactory) {
+        return editableDirectiveFactory({
+          directiveName: directiveName,
+          inputTpl: '<input type="'+type+'">'
+        });
+    }]);
+  });
+
+  //`range` is bit specific
+  angular.module('xeditable').directive('editableRange', ['editableDirectiveFactory',
+    function(editableDirectiveFactory) {
+      return editableDirectiveFactory({
+        directiveName: 'editableRange',
+        inputTpl: '<input type="range" id="range" name="range">',
+        render: function() {
+          this.parent.render.call(this);
+          this.inputEl.after('<output>{{$data}}</output>');
+        }
+      });
+  }]);
+
+  //'Currency' is specific too
+
+  angular.module('xeditable').directive('editableCurrency', ['editableDirectiveFactory',
+    function(editableDirectiveFactory) {
+      return editableDirectiveFactory({
+        directiveName: 'editableCurrency',
+        inputTpl: '<input type="text" ng-currency-mask>'
+      });
+  }]);
+
+}());
